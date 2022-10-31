fbf62121aefa47e71d52f1b6090c6c27544837ad
Bello2288 <bellofattogregg@gmail.com>
2022-10-31 11:15:34 -0400

    fixed twilio.py to put auth_token and account_sid in .env file

--- sources/twilio.py
+++ sources/twilio.py
@@ -2,24 +2,18 @@ import os
 from twilio.rest import Client
 
 
-# account_sid = os.environ['TWILIO_ACCOUNT_SID'] 
-account_sid = 'ACac91b05c269eaffc211556683ccb7d86'
-# auth_token = os.environ['TWILIO_AUTH_TOKEN'] 
-auth_token = '0a1a8de0b8f4fb48d1ac05d9bf793b66'
-# my_phone_number = os.environ['MY_PHONE_NUMBER']   
-# twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']  
+account_sid = os.environ['TWILIO_ACCOUNT_SID'] 
+auth_token = os.environ['TWILIO_AUTH_TOKEN'] 
+my_phone_number = os.environ['MY_PHONE_NUMBER']   
+twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']  
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
     # the creator's phone number (mine for testing)
-    # to=my_phone_number,
-    to='+18646268863',
-
+    to=my_phone_number,
     # from the phone number set up in my free Twilio account
-    # from_=twilio_phone_number,
-    from_='+13609941205',
-
+    from_=twilio_phone_number,
     body="Hello from Python!"
 )
 print('sent')
