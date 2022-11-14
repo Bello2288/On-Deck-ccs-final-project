58e63481fc37958c224014a205221231eb4a8d13
Bello2288 <bellofattogregg@gmail.com>
2022-11-13 20:34:00 -0500

    completed styling for small scale --- Footer and Homepage

--- frontend/static/src/components/HomePage/HomePage.js
+++ frontend/static/src/components/HomePage/HomePage.js
@@ -46,11 +46,6 @@ function Home() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-
-      {/* <div className="home-container">
-        <img className="homepic" src={homepic} alt="home-pic" />
-        <h3 className="home-page-text">Welcome to On Deck<br></br><br></br>The sports app that lets you still play around your busy schedule</h3>
-      </div> */}
       <div className="home-lower-buttons">
         <Nav.Link className="home-create-account btn-hover" href="/register">Create Account</Nav.Link>
         <Nav.Link className="home-login btn-hover" href="/login">Login</Nav.Link>
