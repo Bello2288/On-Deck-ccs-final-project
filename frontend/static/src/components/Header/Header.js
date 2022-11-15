7fb107b33db10b125c910024661987a33ef5b3d9
Bello2288 <bellofattogregg@gmail.com>
2022-11-14 22:32:22 -0500

    fixed Header for mobil scale

--- frontend/static/src/components/Header/Header.js
+++ frontend/static/src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import "../../styles/Header.css"
 // import { useState } from "react";
+import Button from 'react-bootstrap/Button';
 import Container from "react-bootstrap/Container";
+import Form from 'react-bootstrap/Form';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { useNavigate } from "react-router-dom";
 // import football from "../../images/football.png";
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 function Header({ superState, logoutUser }) {
@@ -19,7 +21,7 @@ function Header({ superState, logoutUser }) {
 
   return (  
     <>
-      <Navbar className="navbar" expand="lg">
+      {/* <Navbar className="navbar" expand="lg">
         <Container className="navbar-container nav-flex">
           <Navbar.Brand className="app-name navbar_lg" href="/">
             On Deck
@@ -55,7 +57,7 @@ function Header({ superState, logoutUser }) {
               </>
               )}
             </Nav>
-          </div>
+          </div> */}
 
           {/* <>
             {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
@@ -114,10 +116,87 @@ function Header({ superState, logoutUser }) {
           </> */}
 
 
-        </Container>
-      </Navbar>
+      {/* //   </Container>
+      // </Navbar> */}
+
+
+      {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
+        <Navbar className="mb-3 navbar" key={expand} expand={expand}>
+          <Container className="navbar-container nav-flex" fluid>
+            <Navbar.Brand className="app-name navbar_lg" href="/">
+              On Deck
+            </Navbar.Brand>
+            <div>
+              <img className="profile-picture-small-scale" src={superState.avatar} alt="profile pic" />
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            </div>
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header className="canvas-header" closeButton>
+                {/* <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                </Offcanvas.Title> */}
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <div id="basic-navbar-nav">
+                  <Nav className="me-auto navbar_lg">
+                    {!superState.auth && (
+                      <>
+                        <Nav.Link className="header-login" href="/login">Login</Nav.Link>
+                      </>
+                    )}
+                    {superState.auth && !superState.admin && (
+                      <>
+                        <Nav.Link autoFocus className="header-text" href="/posts">Game Posts</Nav.Link>
+                        <Nav.Link className="header-text" href="/create">Create Post</Nav.Link>
+                        <Nav.Link className="header-text" href="/posts/user">My Posts</Nav.Link>
+                      </>
+                    )}
+                    {superState.admin && (
+                      <>
+                        <Nav.Link className="header-text" href="/posts/editor">Review Posts</Nav.Link>
+                        <Nav.Link className="header-text" href="/" onClick={(e) => logout(e)}>Logout</Nav.Link>
+                      </>
+                    )}
+                    {superState.auth && !superState.admin && (
+                    <>
+                      <NavDropdown className="dropdown" title={superState.username} id="nav-dropdown" variant="primary">
+                        <NavDropdown.Item className="dropdown-opt bg-color" href={`/user/profile/${superState.userID}`}>View Profile</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item className="dropdown-opt bg-color" href="/" onClick={(e) => logout(e)}>Logout</NavDropdown.Item>
+                      </NavDropdown>
+                      <img className="profile-picture-large-scale" src={superState.avatar} alt="profile pic" />
+                    </>
+                    )}
+                  </Nav>
+                </div>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </>
   );
 }
 
 export default Header;
+
+
+
+{/* <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown> */}
